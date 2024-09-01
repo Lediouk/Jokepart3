550f2dcae38cdcd685ab79b1f7172a665963b400
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-08-31 20:01:50 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,15 +3,19 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// modify these settings according to the account on your database server.
-$host = "localhost";
-$port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokesdb";
+// Get database URL from environment variables
+$dbUrl = parse_url(getenv("DATABASE_URL"));
 
+// Extract database connection details
+$host = $dbUrl["host"];
+$username = $dbUrl["user"];
+$user_pass = $dbUrl["pass"];
+$database_in_use = ltrim($dbUrl["path"], '/');
 
+// Create a new mysqli connection
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+
+// Check connection
 if ($mysqli->connect_error) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
